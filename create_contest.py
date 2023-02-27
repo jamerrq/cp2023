028f2e9fe047933630ac81b13d79b3e8cbf4d2e1
Jamer Rq <jamerrq@gmail.com>
2023-02-27 10:19:14 -0500

    created templates folder | via Alekhine

--- create_contest.py
+++ create_contest.py
@@ -3,20 +3,19 @@ import os
 
 nps = 3 # change
 contest_ = "round_853_div2" # change
+platform = "Codeforces" # Change (maybe)
 
 ###
-
-platform = "Codeforces"
 global_p = f"{platform}/{contest_}"
 
 os.system(f'mkdir {platform}/{contest_}')
 
 abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-tmp = "template.py"
+tmp = "./templates/template.py"
 
 for i in range(nps):
     os.system(f'cp {tmp} {global_p}/{abc[i]}.py')
 
 
-rmd = "Codeforces/Unsolved/readme.md"
+rmd = "templates/readme.md"
 os.system(f"cp {rmd} {global_p}")
